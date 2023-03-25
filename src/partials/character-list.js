70f7473c2b0a0218128c515e89d0766d92de4876
megazear7 <alexlockahrt7@gmail.com>
2023-03-25 09:21:08 -0400

    added character status

--- src/partials/character-list.js
+++ src/partials/character-list.js
@@ -4,10 +4,16 @@ export default async characters => {
   return html`
     ${characters.map(character => html`
       <div class="character-from-list search-entry">
-        <h4>${character.fields.name}</h4>
+        <h4>${character.fields.name}${status(character)}</h4>
         <div>Level ${character.fields.level} ${character.fields.race} ${character.fields.class}</div>
         <div>${character.fields.player.fields.name}</div>
       </div>
     `)}
   `;
 };
+
+function status(character) {
+  return character.fields.status != 'Alive' ? html`
+    <small>(${character.fields.status})</small>
+  ` : '';
+}
