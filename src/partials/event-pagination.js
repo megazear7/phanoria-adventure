2e378ecc79099dd480fa11404dabb168f6176e19
Alex Lockhart <alexlockhart7@protonmail.com>
2025-12-28 09:43:39 -0500

    Fixed event pagination

--- src/partials/event-pagination.js
+++ src/partials/event-pagination.js
@@ -2,10 +2,10 @@ import { html } from 'orison';
 
 export default (context, page, pages) => html`
     <div class="pagination-links">
-    <a href="${context.path}/page-${page.page - 1}" class="${page.page > 1 ? '' : 'disabled'}">&lt;</a>
+    <a href="${context.path}/page-${page.page - 1}.html" class="${page.page > 1 ? '' : 'disabled'}">&lt;</a>
     ${pages.map(innerPage => html`
-        <a href="${context.path}/page-${innerPage.page}">${innerPage.page}</a>
+        <a href="${context.path}/page-${innerPage.page}.html" class="${innerPage.page === page.page ? 'active' : ''}">${innerPage.page}</a>
     `)}
-    <a href="${context.path}/page-${page.page + 1}" class="${page.page < pages.length ? '' : 'disabled'}">&gt;</a>
+    <a href="${context.path}/page-${page.page + 1}.html" class="${page.page < pages.length ? '' : 'disabled'}">&gt;</a>
     </div>
 `;
