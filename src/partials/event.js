8f51814ea06f60806513198ab467ccbb1f4d390d
Alex Lockhart <alexlockhart7@protonmail.com>
2026-01-18 09:06:10 -0500

    Removed perspective text

--- src/partials/event.js
+++ src/partials/event.js
@@ -6,19 +6,12 @@ import { soundIcon } from './icon.sound.js';
 export default event => {
     if (!event) return '';
 
-    const chars = event.fields.involvement;
-
-    const singleCharHtml = chars && chars.length === 1 ? html`
-        <p><i>What follows is told from the perspective of ${chars[0].fields.name}</i></p>
-    ` : '';
-
     return html`
         <div class="search-entry">
             <h4>${event.fields.title}${event.fields.songUrl ? html`
                 <button class="sound-icon open-song" song-url="${event.fields.songUrl}">${soundIcon}</button>
             ` : ''}</h4>
             ${datePartial(event.fields.year, event.fields.month, event.fields.day)}
-            ${singleCharHtml}
             ${renderRichText(event.fields.description)}
         </div>
     `;
